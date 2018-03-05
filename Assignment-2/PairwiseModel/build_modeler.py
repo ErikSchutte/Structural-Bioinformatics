ba786cbd14c788fea66c781dd0b03d329336a906
Schutte <e2.schutte@student.vu.nl>
2018-03-05 11:49:55 +0100

    PSA Models

---
+++ Assignment-2/PairwiseModel/build_modeler.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from modeller import *
+from modeller.automodel import *
+
+env = environ()
+
+a = automodel(env,
+	alnfile='template-2LRU.ali',
+	knowns='2LRU-2',
+	sequence='T0882',
+	assess_methods=(assess.DOPE, assess.GA341))
+
+a.starting_model = 1
+a.ending_model = 5
+a.make()
